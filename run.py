12910e95e22b77c7876669bf933bf18bef066fee
Mair Edmunds <xmairx@hotmail.co.uk>
2024-01-23 15:28:25 +0000

    Initial commit - Making basic index.html and addrecipe.html linked to base.html

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/addrecipe")
+def addrecipe():
+    return render_template("addrecipe.html")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True) ##Update to Flase before submitting##
