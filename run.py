1570039fd9eed09726b39f086bf5dce60a3c20b3
Mair Edmunds <xmairx@hotmail.co.uk>
2024-01-25 13:12:39 +0000

    Add 1st recipe to run.py

--- run.py
+++ run.py
@@ -26,3 +26,57 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True) ##Update to False before submitting##
+
+recipes = [ #Add prep, cook time?#
+    {
+        'id': 1,
+        'name'= 'Banana Bread',
+        'serves'= '10',
+        'ingredients': ['3 very ripe medium bananas (approx. 225g peeled)', '3 large eggs', '100g light brown sugar', 
+        '150ml vegetable oil', '275g white self-raising flour', '1 tsp ground mixed spice', '1 tsp baking powder'],
+        'instructions': ['', '', '', ''],
+        'tools': 'Preheat oven at 180 degrees',
+        'cuisine': ['Vegetarian', 'Baking'],
+    },
+    {
+        'id': 2.
+        'name'= ''
+        'serves'= ''
+        'ingredients': ''
+        'tools': ''
+        'cuisine': ''
+    },
+    {
+        'id': 3.
+        'name'= ''
+        'serves'= ''
+        'ingredients': ''
+        'tools': ''
+        'cuisine': ''
+    },
+    {
+        'id': 4.
+        'name'= ''
+        'serves'= ''
+        'ingredients': ''
+        'tools': ''
+        'cuisine': ''
+    },
+    {
+        'id': 5.
+        'name'=''
+        'serves'= ''
+        'ingredients': ''
+        'tools': ''
+        'cuisine': ''
+    },
+    {
+        'id': 6.
+        'name'= ''
+        'serves'= ''
+        'ingredients': ''
+        'tools': ''
+        'cuisine': 'Vegetarian'
+    },
+
+]
