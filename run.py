a03d5ae6204f6457ba6bb14838e85c1ed9e57828
Mair Edmunds <xmairx@hotmail.co.uk>
2024-01-30 18:02:49 +0000

    Remove added taskmanaer folder

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from taskmanager import app
 
-if__name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG"),
-    )
+
+app = Flask(__name__)
 
 
 recipes = [ #Add prep/cook time, submitted by#
