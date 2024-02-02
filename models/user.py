ade368e46acc0de343a8d10531d79160d7975844
Mair Edmunds <xmairx@hotmail.co.uk>
2024-02-02 17:38:50 +0000

    Update py files as per python linter -adding new line at end and checking indents

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from . import db
 
 db = SQLAlchemy()
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
