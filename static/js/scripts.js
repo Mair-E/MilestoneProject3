7e79b2cbf6c13831061f9adf5a0e3236e5e4d819
Mair Edmunds <xmairx@hotmail.co.uk>
2024-02-04 19:24:03 +0000

    seperate js from index.html to scripts.js. Add notes to index.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,5 +3,19 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// Smooth scrolling using jQuery
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
